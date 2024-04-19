0f09122c93f5960b1b096f56cfc203d7b6ffe0dd
Cyruskabir <ariansepahi@yahoo.com>
2024-04-19 06:02:24 +0330

    :bug: fix json server

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api/": {
-        target: "http://localhost:8000",
+        target: "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
